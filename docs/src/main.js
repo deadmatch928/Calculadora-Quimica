cf470e445f78828626adf7c1dc3c0af121afa797
deadmatch928 <titipatipoy@gmail.com>
2025-06-22 17:32:49 -0400

    final checks

--- docs/src/main.js
+++ docs/src/main.js
@@ -77,8 +77,7 @@ window.Calculator = {
         "Se usa frecuentemente en la preparación de medicamentos y soluciones intravenosas.",
       imageUrl: "src/assets/maxresdefault.jpg",
       imageCaption: "Preparación de soluciones en un laboratorio clínico",
-      secondImageUrl:
-        "https://deadmatch928.github.io/Calculadora-Quimica/src/assets/muestreo.jpg",
+      secondImageUrl: "Calculadora-Quimica/src/assets/muestreo.jpg",
       secondImageCaption: "Análisis de concentraciones en muestras de agua",
     },
     "v-v": {
